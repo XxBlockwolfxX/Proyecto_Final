f35cd000ca55b41be54b75083916fb9f12b22922
XxBlockwolfxX <163485976+XxBlockwolfxX@users.noreply.github.com>
2024-07-08 22:41:34 -0500

    g

--- Proyecto/models/Pacientes.models.php
+++ Proyecto/models/Pacientes.models.php
@@ -1,2 +1,111 @@
 <?php
-    require_once("../config/conexion.php");
+require_once('../config/conexion.php');
+
+class Clase_Paciente
+{
+    public function probar()
+    {
+        $con = new Clase_Conectar();
+        $con = $con->Procedimiento_Conectar();
+        echo $con->error;
+    }
+
+    public function todos()
+    {
+        try {
+            $con = new Clase_Conectar();
+            $con = $con->Procedimiento_Conectar();
+            $cadena = "SELECT * FROM pacientes";
+            $resultado = mysqli_query($con, $cadena);
+            $con->close();
+            return $resultado;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function uno($id)
+    {
+        try {
+            $con = new Clase_Conectar();
+            $con = $con->Procedimiento_Conectar();
+            $cadena = "SELECT * FROM pacientes WHERE id_paciente = $id";
+            $resultado = mysqli_query($con, $cadena);
+            return $resultado;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        } finally {
+            $con->close();
+        }
+    }
+
+    public function insertar($nombre, $apellido, $edad, $direccion)
+    {
+        try {
+            $con = new Clase_Conectar();
+            $con = $con->Procedimiento_Conectar();
+            $cadena = "INSERT INTO `pacientes`(`nombre`, `apellido`, `edad`, `direccion`) VALUES ('$nombre','$apellido',$edad,'$direccion')";
+            if (mysqli_query($con, $cadena)) {
+                return "ok";
+                $con->close();
+            } else {
+                return $con->error;
+                $con->close();
+            }
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function actualizar($id, $nombre, $apellido, $edad, $direccion)
+    {
+        try {
+            $con = new Clase_Conectar();
+            $con = $con->Procedimiento_Conectar();
+            $cadena = "UPDATE `pacientes` SET `nombre`='$nombre',`apellido`='$apellido',`edad`=$edad,`direccion`='$direccion' WHERE `id_paciente`=$id";
+            if (mysqli_query($con, $cadena)) {
+                return "ok";
+            } else {
+                return $con->error;
+            }
+        } catch (Exception $e) {
+            return $e->getMessage();
+        } finally {
+            $con->close();
+        }
+    }
+
+    public function eliminar($id)
+    {
+        try {
+            $con = new Clase_Conectar();
+            $con = $con->Procedimiento_Conectar();
+            $cadena = "DELETE FROM pacientes WHERE id_paciente = $id";
+            if (mysqli_query($con, $cadena)) {
+                return "ok";
+            } else {
+                return $con->error;
+            }
+        } catch (Exception $e) {
+            return $e->getMessage();
+        } finally {
+            $con->close();
+        }
+    }
+
+    public function buscarXNombre($nombre)
+    {
+        try {
+            $con = new Clase_Conectar();
+            $con = $con->Procedimiento_Conectar();
+            $cadena = "SELECT * FROM pacientes WHERE nombre = '$nombre'";
+            $resultado = mysqli_query($con, $cadena);
+            return $resultado;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        } finally {
+            $con->close();
+        }
+    }
+}
+?>
